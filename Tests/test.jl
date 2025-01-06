1706eece113637a20c3e0c8f83a5c4fb38ae9a2c
Nurdaylight <157308256+Nurdaylight@users.noreply.github.com>
2025-01-06 21:25:11 +0500

    Update test.jl

--- Tests/test.jl
+++ Tests/test.jl
@@ -1,1 +1,26 @@
-
+using Test
+begin
+ # Sample parameters
+ a = 150     # Number of successes
+ b = 500     # Number of trials
+ c = 0.3     # Hypothesized probability
+ 
+ # 1. Two-sided test
+ @testset "Two-sided BinomialTest" begin
+     test = BinomialTest(a, b, c)
+     @test isapprox(pvalue(test), 1, atol=1e-3) 
+ end
+ 
+ # 2. One-sided test (greater)
+ @testset "One-sided BinomialTest (greater)" begin
+     test = BinomialTest(a, b, c)
+     @test isapprox(pvalue(test, tail= :right), 0.5168, atol=1e-3) 
+ end
+ 
+ # 3. One-sided test (less)
+ @testset "One-sided BinomialTest (less)" begin
+     test = BinomialTest(a, b, c)
+     @test isapprox(pvalue(test, tail=:left), .52204, atol=1e-3)
+ end
+ 
+end
